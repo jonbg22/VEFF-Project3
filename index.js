2d1e7977c1c30614292e0cb8a20ddb0c3317bb2e
jonbg22 <jonbg22@ru.is>
2023-03-20 17:19:05 +0000

    finished

--- index.js
+++ index.js
@@ -66,18 +66,21 @@ const genres = [
   { id: "1", genreName: "Classic" },
 ];
 
+let nextTuneId = 2;
+let nextGenreId = 2;
+
 //Your endpoints go here
 const apiPath = "/api/v1/"
 
 
 // GET REQUESTS ========
 
-app.get(apiPath + "tunes/:filter?",(req,res) => {
+app.get(apiPath + "tunes/",(req,res) => {
   let responseArr = [];
-  let filterId = req.params.filter
+  let filteredGenre = req.query.filter ? genres.find(genre => genre.genreName.toLowerCase() == req.query.filter.toLowerCase()) : undefined
 
   tunes.forEach(({id,name,genreId}) => {
-    if (genreId == filterId || filterId == undefined) {
+    if (filteredGenre == undefined  || genreId == filteredGenre.id) {
     responseArr.push({
       id,
       name,
@@ -86,12 +89,12 @@ app.get(apiPath + "tunes/:filter?",(req,res) => {
   }
   });
 
-  res.status(201).json(responseArr);
+  res.status(200).json(responseArr);
 });
 
 
 app.get(apiPath + "genres",(req,res) => {
-  res.status(201).json(genres);
+  res.status(200).json(genres);
 })
 
 
@@ -100,35 +103,93 @@ app.get(apiPath + "genres/:genreId/tunes/:tuneId",(req,res) => {
   const genreId = req.params.genreId;
 
   const reqTune = tunes.find(tune => tune.id == tuneId && tune.genreId == genreId);
-
-  res.status(201).json(reqTune);
-
-
+  if (!reqTune) return res.status(404).json({message: "Tune not found"});
+  res.status(200).json(reqTune);
 })
 
 // POST REQUESTS =======
+
+// New Tune
 app.post(apiPath + "genres/:genreId/tunes",(req,res) => {
-  const genreId = req.params.genreId;
+  const genId = req.params.genreId;
   // Verifies that the genre exists
-  if (!genres.some(genre => genre.id == genreId )) return res.status(400).json({message: "Specified genre does not exist"})
+  if (!genres.some(genre => genre.id == genId )) return res.status(400).json({message: "Specified genre does not exist"})
   else if (
     !req.body ||
     !req.body.name ||
-    !req.body.content
+    !req.body.content 
   ) return res.status(400).json({ message: "Tune needs a name and content",});
+  else if (!req.body.content.every(({note,duration,timing}) => {
+    return note && duration && timing && typeof(duration) == "string"
+  })) res.status(400).json({message: "Content needs to be a non empty array of objects with note, duration and timing properties"})
   else { 
-    // TODO 
+    const newTune = {
+      id: nextTuneId.toString(),
+      name: req.body.name.toString(),
+      genreId: genId.toString(),
+      content: req.body.content
+      };
+
+    tunes.push(newTune);
+    nextTuneId++;
+    res.status(201).json(newTune);
   }
 });
 
+// New Genre
+app.post(apiPath + "genres",(req,res) => {
+  if (!req.body || !req.body.genreName) return res.status(400).json({message: "Genre needs a name"});
+  else if (genres.some(genre => genre.genreName == req.body.genreName)) return res.status(400).json({message: "Genre already exists"});
+  else {
+    const newGenre = {
+      id: nextGenreId.toString(),
+      genreName: req.body.genreName.toString()
+    };
+
+    genres.push(newGenre);
+    nextGenreId++;
+    res.status(201).json(newGenre);
+  }
+})
 
 
 
+// PATCH REQUESTS ========
+app.patch(apiPath + "genres/:genreId/tunes/:tuneId",(req,res) => {
+  const selectedIndex = tunes.findIndex(t => t.genreId == req.params.genreId && t.id == req.params.tuneId );
+  const selectedTune = tunes[selectedIndex];
+  
+  if (selectedTune == undefined) res.status(400).json({"message":"No tune found with specified id and genreId"});
+  else if (!genres.some(genre => genre.id == req.body.genreId)) res.status(400).json({"message":"Updated genre does not exist"});
+  else {
+    if (req.body.name) selectedTune.name = req.body.name.toString();
+    if (req.body.genreId) selectedTune.genreId = req.body.genreId.toString();
+    if (req.body.content) selectedTune.content = req.body.content;
+  }
+
+  res.status(200).json(selectedTune);
+})
+
+
+// DELETE REQUESTS ========
+app.delete(apiPath + "genres/:genreId",(req,res) => {
+  const genreId = req.params.genreId;
+  const genreIndex = genres.findIndex(genre => genre.id == genreId);
+  if (genreIndex === -1) return res.status(400).json({message: "Specified genre does not exist"});
+  else if (tunes.some(tune => tune.genreId == genreId)) return res.status(400).json({message: "Cannot delete non-empty genre"});
+  else {
+    const genreIndex = genres.findIndex(genre => genre.id == genreId);
+     const genre = genres.splice(genreIndex,1);
+    res.status(200).json(genre);
+  }
+})
 
 // Default path for unsupported actions
-app.get("*", (req, res) => {
-    res.status(405).send("Operation not supported");
+app.use("*", (req, res) => {
+    res.status(405).json({ message: "Operation not supported" });
 });
+
+
 
 //Start the server
 app.listen(port, () => {
